cb278cb91c01240c53361ed9598e0f5d76bf2fea
Armaan Shaikh <shaikharmaan1073@gmail.com>
2025-08-28 23:00:42 +0530

    size selection done

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,9 +1,15 @@
-import { View, Text, Image , StyleSheet} from 'react-native';
+import { View, Text, Image , StyleSheet, Pressable} from 'react-native';
 import React from 'react';
 import { useLocalSearchParams, Stack } from 'expo-router';
 import products from '@/assets/data/products';
+import { useState } from 'react';
+
+const sizes = ['S', 'M', 'L', 'XL'];
 
 const ProductDetailsScreen = () => {
+  //const [selectedSize, setSelectedSize] = useState('M');
+  const [selectedSize, setSelectedSize] = useState('M');
+
   const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png';
 
   const { id } = useLocalSearchParams<{ id: string }>();
@@ -20,6 +26,31 @@ const ProductDetailsScreen = () => {
       <Image 
         source={{uri: product.image || defaultPizzaImage}} 
         style={Styles.image} />
+
+      <Text>Select Size</Text>
+
+      <View style={Styles.sizes}>
+          {sizes.map((size) => (
+            <Pressable 
+            onPress={() => {
+              setSelectedSize(size);
+            }}
+            style={[
+              Styles.size, 
+              {
+                backgroundColor: selectedSize === size ? 'gainsboro' : 'white',
+              }
+            ]} key={size}>
+              <Text style={[Styles.sizeText,
+                {
+                  color: selectedSize === size ? 'black' : 'gainsboro',
+                }
+              ]} >{size}</Text>
+            </Pressable>
+          ))}
+      </View>
+      
+
       <Text style={Styles.price}>${product.price}</Text>
     </View>
   );
@@ -38,7 +69,24 @@ const Styles = StyleSheet.create({
   price: {
     fontSize: 18,
     fontWeight: 'bold'
-  }
+  },
+  sizes: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginVertical: 10,
+  },
+ size: {
+    backgroundColor: 'gainsboro',
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: 500,
+  },
 });
 
 export default ProductDetailsScreen;
