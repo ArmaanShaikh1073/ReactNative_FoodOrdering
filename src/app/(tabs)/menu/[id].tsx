2abe72a0864fa6cd1781e1bafb3977da29df4eee
Armaan Shaikh <shaikharmaan1073@gmail.com>
2025-08-28 20:23:23 +0530

    started dynamic product details page

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,17 +1,44 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image , StyleSheet} from 'react-native';
 import React from 'react';
 import { useLocalSearchParams, Stack } from 'expo-router';
+import products from '@/assets/data/products';
 
 const ProductDetailsScreen = () => {
+  const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png';
+
   const { id } = useLocalSearchParams<{ id: string }>();
 
+  const product = products.find((p) => p.id.toString() === id);
+
+  if(!product){
+    return <Text>Product not found</Text>
+  }
   return (
-    <View>
+    <View style={Styles.container}>
       {/* Correct usage of Stack.Screen */}
-      <Stack.Screen options={{ title: 'Details' }} />
-      <Text>ProductDetailsScreen id: {id}</Text>
+      <Stack.Screen options={{ title: product.name}} />
+      <Image 
+        source={{uri: product.image || defaultPizzaImage}} 
+        style={Styles.image} />
+      <Text style={Styles.price}>${product.price}</Text>
     </View>
   );
 };
+
+const Styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 10
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+});
 
 export default ProductDetailsScreen;
